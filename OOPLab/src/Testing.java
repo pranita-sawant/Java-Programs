038a322f8297d383ab85dfbac71bd9ff875238ce
Pranita Sawant <pranita@2paws.xyz>
2018-07-07 20:01:48 +0530

    First Project to test : Connect eclipse to Github

---
+++ OOPLab/src/Testing.java
@@ -0,0 +1,10 @@
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello This is my first project to test:\n");
+		System.out.print("Connect eclipse to Github");
+	}
+
+}
